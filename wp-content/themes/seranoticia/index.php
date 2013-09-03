46b2e7687127a43d88118001530af4a09f07fdd7
Jguajardo <guajardourzua@gmail.com>
2013-09-02 21:47:49 -0400

    index modificado

--- wp-content/themes/seranoticia/index.php
+++ wp-content/themes/seranoticia/index.php
@@ -1,11 +1,8 @@
 <?php get_header();?>
 			
-<<<<<<< HEAD
-<!-- Archivo principal del tema, modificado-->
-
-=======
-<!-- Archivo principal del tema-->
->>>>>>> cfb6cf3c8f2026ba2d219b567753a597191a6af6
+ 
+<!-- Archivo principal del tema, modificado 2-->
+ 
 		<section id="main">
 			<?php get_template_part('loop','index'); ?>
 		</section>
